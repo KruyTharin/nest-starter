1b120a443c0767165ee9009562ad519753a6d7ca
tharin.kruy <tharin.kruy@wingbank.com.kh>
2026-02-17 14:41:15 +0700

    feat: add docs api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 
+@ApiTags('app')
 @Controller()
 export class AppController {
   constructor(
@@ -10,11 +12,15 @@ export class AppController {
   ) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get hello message' })
+  @ApiResponse({ status: 200, description: 'Return hello message.' })
   getHello(): string {
     return this.appService.getHello();
   }
 
   @Get('users')
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: 200, description: 'Return all users.' })
   async findAll() {
     return await this.prismaService.user.findMany();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,47 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Bootstrap');
 
-  console.log(`Application is running on: ${process.env.PORT ?? 3000}`);
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.APP_PORT ?? 5000;
+  const appVersion = process.env.APP_VERSION ?? 'v1';
+  const baseUrl = process.env.BASE_URL ?? `http://localhost:${port}`;
+
+  app.enableCors();
+  app.setGlobalPrefix(`/api/${appVersion}`);
+
+  const config = new DocumentBuilder()
+    .setTitle(process.env.APP_NAME ?? 'NestJS Starter API')
+    .setDescription('The API description')
+    .setVersion(process.env.APP_VERSION ?? '1.0')
+    .addTag('nest')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Standard Swagger UI
+  SwaggerModule.setup('docs', app, document);
+
+  // Scalar API Reference
+  app.use(
+    '/reference',
+    apiReference({
+      spec: {
+        content: document,
+      },
+    }),
+  );
+
+  await app.listen(port);
+
+  logger.log(`🚀 Application is running on: ${baseUrl}`);
+  logger.log(`📚 Swagger UI: ${baseUrl}/api/docs`);
+  logger.log(`📑 Scalar UI: ${baseUrl}/api/reference`);
 }
 bootstrap();
